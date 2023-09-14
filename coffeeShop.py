f5d110454d31b6ec098aa208926dc20f96db0a25
Delano Marques <54899930+delanobmarques@users.noreply.github.com>
2023-09-14 15:08:19 -0300

    tech-check template

---
+++ coffeeShop.py
@@ -0,0 +1,18 @@
+# Author: Student Name
+# Date: September 14, 2023
+# Title: Cofee Shop - Teck-check #1
+# Description: Console-based Python program that will simulate a coffee shop order.
+                # Calculate the amount of the tip, the tax, and the total amount of a coffee shop, 
+                # considering the cost per cup is $1.80. The program will prompt the user to input 
+                # the number of cups they wish to order, and the tip percentage. The program will then output: 
+                # 1) Total cost before tax, 2) Tax amount, 3) Tip amount (with percentage entered by the user), 
+                # and 4) Final Cost of the bill, including tax and tip.
+
+def main():#don't change this line
+    # Your code should START here
+
+
+    # Your code should END here
+
+if __name__ == "__main__": #don't change this line
+    main()                  #don't change this line
